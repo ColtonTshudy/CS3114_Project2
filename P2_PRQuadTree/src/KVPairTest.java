72042bd7f79dce2ce46e46c7b5798170a921d44d
bengallini <bengallini@vt.edu>
2022-09-23 16:21:07 -0400

    KVpair test

--- P2_PRQuadTree/src/KVPairTest.java
+++ P2_PRQuadTree/src/KVPairTest.java
@@ -1,4 +1,62 @@
+// Virginia Tech Honor Code Pledge:
+//
+// As a Hokie, I will conduct myself with honor and integrity at all times.
+// I will not lie, cheat, or steal, nor will I accept the actions of those who
+// do.
+// -- Benjamin Gallini (bengallini)
+// -- Colton Tshudy
 
-public class KVPairTest {
+import student.TestCase;
 
+/**
+ * KVPair test class
+ * 
+ * @author Benjamin Gallini
+ * @author Colton Tshudy
+ * @version 9/23/2022
+ */
+public class KVPairTest extends TestCase {
+    private KVPair<String, String> pair;
+
+    /**
+     * Sets up the tests
+     */
+    public void setUp() {
+        pair = new KVPair<String, String>("A", "apple");
+    }
+    
+    /**
+     * Tests compareTo method
+     */
+    public void testCompareTo() {
+        KVPair<String, String> pair2 = new KVPair<String, String>("B",
+            "banana");
+        assertTrue(pair.compareTo(pair2) < 0);
+        assertTrue(pair2.compareTo(pair) > 0);
+        assertEquals(pair2.compareTo(pair2), 0);
+        assertTrue(pair.compareTo("B") < 0);
+        assertTrue(pair2.compareTo("A") > 0);
+        assertEquals(pair2.compareTo("B"), 0);
+    }
+
+    /**
+     * Tests key method
+     */
+    public void testKey() {
+        assertEquals(pair.key(), "A");
+    }
+
+    /**
+     * Tests value method
+     */
+    public void testValue() {
+        assertEquals(pair.value(), "apple");
+    }
+
+    /**
+     * Tests toString method
+     */
+    public void testToString() {
+        assertEquals(pair.toString(), "A, apple");
+    }
 }
